afc21167117887f397bc09fc04ccd5b0737ade1c
Vibodh Nautiyal <vibodh.nautiyal_ug22@ashoka.edu.in>
2022-05-02 14:28:35 +0530

    Updates

---
+++ precache-manifest.61858a3d55464cb87e803fedcb2ff225.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6f02bccfb65614b213b",
+    "url": "/react-gh-pages/static/css/main.39c61ea2.chunk.css"
+  },
+  {
+    "revision": "d6f02bccfb65614b213b",
+    "url": "/react-gh-pages/static/js/main.d6f02bcc.chunk.js"
+  },
+  {
+    "revision": "18c57329505def31d83c",
+    "url": "/react-gh-pages/static/js/1.18c57329.chunk.js"
+  },
+  {
+    "revision": "d44bc536ede991d08320",
+    "url": "/react-gh-pages/static/js/2.d44bc536.chunk.js"
+  },
+  {
+    "revision": "9697933c7ac2e61f4d9a",
+    "url": "/react-gh-pages/static/js/runtime~main.9697933c.js"
+  },
+  {
+    "revision": "8e9d975798d2224f0503d38d22589b38",
+    "url": "/react-gh-pages/static/media/randomness1.8e9d9757.jpg"
+  },
+  {
+    "revision": "d1bad23c49d8a8c073da7c6f77407258",
+    "url": "/react-gh-pages/static/media/randomness2.d1bad23c.jpg"
+  },
+  {
+    "revision": "a35b90332d8f78efc2037bbddd82f8e2",
+    "url": "/react-gh-pages/static/media/bg3.a35b9033.jpg"
+  },
+  {
+    "revision": "ada8f1e242d669b96eeb4cb84e168423",
+    "url": "/react-gh-pages/index.html"
+  }
+];
